bc88d0fbef5223110d64db1576d7234793436b5c
ReyesAerold <reyesaeroldsteven1604@gmail.com>
2024-12-18 13:30:59 +0800

    first commit

---
+++ Scroll_Text_Reyes_2B/app/src/main/java/com/example/Text_scroll_reyes_2B/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.Text_scroll_reyes_2B;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.scroll_text_floresca_2b.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
